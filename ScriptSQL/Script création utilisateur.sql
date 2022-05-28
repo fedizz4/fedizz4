4f14e50d04c1f1f277d993d91e47e2b9177b7482
Samantha Dhaussy <93337988+samsam86170@users.noreply.github.com>
2022-05-28 16:25:47 +0200

    Update Script création utilisateur.sql

--- ScriptSQL/Script création utilisateur.sql
+++ ScriptSQL/Script création utilisateur.sql
@@ -1,3 +1,3 @@
-CREATE USER 'app_sam'@'%' IDENTIFIED BY 'pwdInactif';
+CREATE USER 'app_sam'@'%' IDENTIFIED BY '@Azerty85!';
 GRANT USAGE ON *.* TO 'app_sam'@'%';
 GRANT ALL PRIVILEGES ON `mediatek86`.* TO 'app_sam'@'%';
